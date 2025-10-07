1fc4913bdeab7645498cee159c29c3f8c23c659a
zaakiraza <raaza.zaakir@gmail.com>
2025-10-07 08:15:16 +0500

    changes

--- Utlis/DB.js
+++ Utlis/DB.js
@@ -5,8 +5,9 @@ dotenv.config();
 export const connectDB = async () => {
     try {
         const mon = await mongoose.connect(process.env.MONGOURI);
+        // const mon = await mongoose.connect(process.env.MONGOURILOCAL);
         console.log("DataBase connected");
     } catch (error) {
-        console.log(error.message);
+        console.log("Error: ",error.message);
     }
 };
